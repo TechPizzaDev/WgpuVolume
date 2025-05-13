5d62850d9511ac3b0c9b031d0e060ead1278f191
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2025-05-13 15:16:38 +0200

    Skip C# build when submodule is missing

--- prepare.ts
+++ prepare.ts
@@ -15,28 +15,39 @@ const { values, positionals } = parseArgs({
   allowPositionals: true,
 });
 
+async function build() {
+  if (!values.regen && await file(outPath).exists()) {
+    console.log(`Noise file exists \n  at "${resolve(outPath)}"`);
+    return;
+  }
+
+  let submodName = "./submodules/SharpFastNoise2/SharpFastNoise2.sln";
+  if (!await file(submodName).exists()) {
+    console.warn(`Submodule file "${submodName}" missing, skipping build.`);
+    return;
+  }
+
+  $.cwd("./generator");
+
+  console.log("Building noise generator...");
+  await $`dotnet build -c Release`;
+
+  console.log("Running noise generator...")
+  let path = await $`dotnet run --no-build -c Release`.text();
+
+  console.log(`Copying Noise file \n  from "${resolve(path)}" \n  to "${resolve(outPath)}"`);
+  await Bun.write(file(outPath), file(path));
+}
+
 try {
-    if (!values.regen && await file(outPath).exists()) {
-        console.log(`Noise file exists \n  at "${resolve(outPath)}"`);
-    } else {
-        $.cwd("./generator");
-
-        console.log("Building noise generator...");
-        await $`dotnet build -c Release`;
-        
-        console.log("Running noise generator...")
-        let path = await $`dotnet run --no-build -c Release`.text();
-
-        console.log(`Copying Noise file \n  from "${resolve(path)}" \n  to "${resolve(outPath)}"`);
-        await Bun.write(file(outPath), file(path));
-    }
+  build();
 } catch (e) {
-    if (e instanceof $.ShellError) {
-        const decoder = new TextDecoder();
-        console.error("ShellError exitCode:", e.exitCode);
-        console.error("ShellError stdout:", decoder.decode(e.stdout));
-        console.error("ShellError stderr:", decoder.decode(e.stderr));
-    } else {
-        console.error(e);
-    }
+  if (e instanceof $.ShellError) {
+    const decoder = new TextDecoder();
+    console.error("ShellError exitCode:", e.exitCode);
+    console.error("ShellError stdout:", decoder.decode(e.stdout));
+    console.error("ShellError stderr:", decoder.decode(e.stderr));
+  } else {
+    console.error(e);
+  }
 }
